290fa5a552fc2309451eb88b3362957c57e1ae01
Cansu Öztaş <fcansuoztas@gmail.com>
2022-08-20 23:39:09 +0300

    Analog-Clock-Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Analog-Clock-Project
+[Click Here to see!](https://cansuoztas.github.io/Analog-Clock-Project/)
+![](https://github.com/Cansuoztas/Analog-Clock-Project/blob/main/Analog.png)
---
+++ app.js
@@ -0,0 +1,14 @@
+const hours = document.querySelector(".hour");
+const min = document.querySelector(".minute");
+const sec = document.querySelector(".second");
+
+function tiktak() {
+  let second = new Date().getSeconds();
+  let minute = new Date().getMinutes();
+  let hour = new Date().getHours();
+
+  sec.style.transform = `rotate(${second * 6}deg)`;
+  min.style.transform = `rotate(${minute * 6}deg)`;
+  hours.style.transform = `rotate(${hour * 30}deg)`;
+}
+setInterval(tiktak, 1000);
